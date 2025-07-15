e686156704d60240bb0306e0133aaa221268d6ae
Takayoshi Makabe <makabe.takayoshi1117@gmail.com>
2025-07-15 10:09:56 +0900

    refactor

--- src/asset_calc.py
+++ src/asset_calc.py
@@ -10,6 +10,31 @@ from typing import Optional
 import numpy as np
 
 
+class Constants:
+    """Constants used in asset calculations"""
+
+    # Percentage conversion
+    PERCENT_TO_DECIMAL = 100
+
+    # Time constants
+    MONTHS_IN_YEAR = 12
+    MAX_YEARS = 20
+
+    # Tax rates
+    CAPITAL_GAINS_TAX_RATE = 0.20315  # Japan capital gains tax rate
+    US_WITHHOLDING_TAX_NISA = 0.9  # US stocks in NISA (after 10% withholding)
+    JP_DIVIDEND_TAX_TAXABLE = 0.79685  # Japan dividend after tax in taxable account
+    US_DIVIDEND_TAX_TAXABLE = 0.71787  # US dividend after tax in taxable account
+
+    # Display constants
+    YEN_UNIT_DIVISOR = 10000  # Convert to 万円 (10,000 yen units)
+
+    # Simulation constants
+    DEFAULT_SIMULATION_TIME = 1000
+    PERCENTILE_DIVISOR = 10
+    RANGE_DIVISOR = 2
+
+
 class Asset:
     """Asset class"""
 
@@ -26,16 +51,18 @@ class Asset:
         no_tax: float,
     ):
         self.name = name
-        self.yld = yld / 100
-        self.div = div / 100
+        self.yld = yld / Constants.PERCENT_TO_DECIMAL
+        self.div = div / Constants.PERCENT_TO_DECIMAL
         self.year = int(year)
         self.reserved = reserved
         self.init_fund = init_fund
         self.is_jp = bool(is_jp)
-        self.volatility = volatility / 100
+        self.volatility = volatility / Constants.PERCENT_TO_DECIMAL
         self.no_tax = bool(no_tax)
-        self.yld_month = (1 + self.yld) ** (1 / 12) - 1
-        self.volatility_month = self.volatility * (1 / math.sqrt(12))
+        self.yld_month = (1 + self.yld) ** (1 / Constants.MONTHS_IN_YEAR) - 1
+        self.volatility_month = self.volatility * (
+            1 / math.sqrt(Constants.MONTHS_IN_YEAR)
+        )
         self._capital_price_transition: Optional[list] = None
         self._price_transition: Optional[list] = None
 
@@ -53,11 +80,11 @@ class Asset:
         return self._price_transition
 
     def set_price_transition(self) -> None:
-        """Set the asset transition when operating for 20 years"""
+        f"""Set the asset transition when operating for {Constants.MAX_YEARS} years"""
         capital_price_transition_month = [self.init_fund]
         price_transition_month = [self.init_fund]
-        for year in range(20):  # max 20 years
-            for month in range(12):
+        for year in range(Constants.MAX_YEARS):
+            for month in range(Constants.MONTHS_IN_YEAR):
                 if year < self.year:
                     capital_price_transition_month.append(
                         capital_price_transition_month[-1] + self.reserved
@@ -74,8 +101,10 @@ class Asset:
                         price_transition_month[-1] * (1 + self.yld_month)
                     )
 
-        self._capital_price_transition = capital_price_transition_month[::12]
-        self._price_transition = price_transition_month[::12]
+        self._capital_price_transition = capital_price_transition_month[
+            :: Constants.MONTHS_IN_YEAR
+        ]
+        self._price_transition = price_transition_month[:: Constants.MONTHS_IN_YEAR]
 
 
 def get_total_transition(assets: list[Asset]) -> dict:
@@ -108,13 +137,19 @@ def get_ratio_asset(assets: list[Asset]) -> dict:
     not_tax = []
     for asset in assets:
         not_tax.append(
-            {'name': asset.name, 'y': asset.price_transition[max_year] // 10000}
+            {
+                'name': asset.name,
+                'y': asset.price_transition[max_year] // Constants.YEN_UNIT_DIVISOR,
+            }
         )
 
         # NoTax(NISA)
         if asset.no_tax:
             has_tax.append(
-                {'name': asset.name, 'y': asset.price_transition[max_year] // 10000}
+                {
+                    'name': asset.name,
+                    'y': asset.price_transition[max_year] // Constants.YEN_UNIT_DIVISOR,
+                }
             )
         # HasTax
         else:
@@ -122,11 +157,12 @@ def get_ratio_asset(assets: list[Asset]) -> dict:
                 asset.price_transition[max_year]
                 - asset.capital_price_transition[max_year]
             )
-            tax = profit * 0.20315
+            tax = profit * Constants.CAPITAL_GAINS_TAX_RATE
             has_tax.append(
                 {
                     'name': asset.name,
-                    'y': (asset.price_transition[max_year] - tax) // 10000,
+                    'y': (asset.price_transition[max_year] - tax)
+                    // Constants.YEN_UNIT_DIVISOR,
                 }
             )
     return {
@@ -135,7 +171,9 @@ def get_ratio_asset(assets: list[Asset]) -> dict:
     }
 
 
-def get_density_dist(assets: list[Asset], simulation_time: int = 1000) -> dict:
+def get_density_dist(
+    assets: list[Asset], simulation_time: int = Constants.DEFAULT_SIMULATION_TIME
+) -> dict:
     """Returns the density distribution of assets"""
     max_year = max([asset.year for asset in assets])
     _result_total = np.zeros(simulation_time)
@@ -147,34 +185,51 @@ def get_density_dist(assets: list[Asset], simulation_time: int = 1000) -> dict:
         for _ in range(simulation_time):
             now_price = asset.init_fund
             random_norm = np.random.normal(
-                loc=asset.yld_month, scale=asset.volatility_month, size=max_year * 12
+                loc=asset.yld_month,
+                scale=asset.volatility_month,
+                size=max_year * Constants.MONTHS_IN_YEAR,
             )
             for year in range(max_year):
-                for month in range(12):
+                for month in range(Constants.MONTHS_IN_YEAR):
                     if year < asset.year:
                         now_price = (
-                            now_price * (1 + random_norm[year * 12 + month])
+                            now_price
+                            * (1 + random_norm[year * Constants.MONTHS_IN_YEAR + month])
                             + asset.reserved
                         )
                     else:
-                        now_price = now_price * (1 + random_norm[year * 12 + month])
+                        now_price = now_price * (
+                            1 + random_norm[year * Constants.MONTHS_IN_YEAR + month]
+                        )
             result.append(now_price - _origin)
 
         _result_total += np.array(result)
 
         _result = sorted(result)
         _idx = bisect_right(_result, 0)
-        _prob = _idx / simulation_time * 100
+        _prob = _idx / simulation_time * Constants.PERCENT_TO_DECIMAL
 
-        _top10 = _result[simulation_time // 10 * 9] // 10000
-        _top30 = _result[simulation_time // 10 * 7] // 10000
-        _worst30 = _result[simulation_time // 10 * 3] // 10000
-        _worst10 = _result[simulation_time // 10] // 10000
+        _top10 = (
+            _result[simulation_time // Constants.PERCENTILE_DIVISOR * 9]
+            // Constants.YEN_UNIT_DIVISOR
+        )
+        _top30 = (
+            _result[simulation_time // Constants.PERCENTILE_DIVISOR * 7]
+            // Constants.YEN_UNIT_DIVISOR
+        )
+        _worst30 = (
+            _result[simulation_time // Constants.PERCENTILE_DIVISOR * 3]
+            // Constants.YEN_UNIT_DIVISOR
+        )
+        _worst10 = (
+            _result[simulation_time // Constants.PERCENTILE_DIVISOR]
+            // Constants.YEN_UNIT_DIVISOR
+        )
 
         table_rows.append(
             {
                 'name': asset.name,
-                'originPrice': _origin // 10000,
+                'originPrice': _origin // Constants.YEN_UNIT_DIVISOR,
                 'top10': (
                     f'+{_top10:.0f}'
                     if _top10 > 0
@@ -204,12 +259,14 @@ def get_density_dist(assets: list[Asset], simulation_time: int = 1000) -> dict:
 
     _min = result_total[0]
     _max = result_total[-1]
-    _range = (_max - _min) // 10
+    _range = (_max - _min) // Constants.PERCENTILE_DIVISOR
 
     result_total = [r - _min for r in result_total]
     result_total = [r // _range * _range for r in result_total]
-    result_total = [r - (_range // 2) + _min for r in result_total]
-    result_total = [r // 10000 for r in result_total]
+    result_total = [
+        r - (_range // Constants.RANGE_DIVISOR) + _min for r in result_total
+    ]
+    result_total = [r // Constants.YEN_UNIT_DIVISOR for r in result_total]
 
     cnt_result = collections.Counter(result_total).items()
     data = [[k, v / simulation_time] for k, v in cnt_result]
@@ -225,7 +282,7 @@ def get_dividend_price(assets: list[Asset]) -> dict:
     for asset in assets:
         for i in range(max_year + 1):
             if i < len(asset.price_transition):
-                p = asset.price_transition[i] * asset.div / 10000
+                p = asset.price_transition[i] * asset.div / Constants.YEN_UNIT_DIVISOR
 
                 # NoTax(NISA)
                 if asset.no_tax:
@@ -234,7 +291,7 @@ def get_dividend_price(assets: list[Asset]) -> dict:
                         prices[i] += p
                     # USStock
                     else:
-                        p1 = p * 0.9
+                        p1 = p * Constants.US_WITHHOLDING_TAX_NISA
                         p2 = p - p1
                         prices[i] += p1
                         tax[i] += p2
@@ -242,13 +299,13 @@ def get_dividend_price(assets: list[Asset]) -> dict:
                 else:
                     # JapanStock
                     if asset.is_jp:
-                        p1 = p * 0.79685
+                        p1 = p * Constants.JP_DIVIDEND_TAX_TAXABLE
                         p2 = p - p1
                         prices[i] += p1
                         tax[i] += p2
                     # USStock
                     else:
-                        p1 = p * 0.71787
+                        p1 = p * Constants.US_DIVIDEND_TAX_TAXABLE
                         p2 = p - p1
                         prices[i] += p1
                         tax[i] += p2
@@ -283,15 +340,15 @@ def get_demolition_price(assets: list[Asset], duration: int) -> dict:
                 yield_year = asset.yld + asset.div
             # USStock
             else:
-                yield_year = asset.yld + asset.div * 0.9
+                yield_year = asset.yld + asset.div * Constants.US_WITHHOLDING_TAX_NISA
         # HasTax
         else:
             # JapanStock
             if asset.is_jp:
-                yield_year = asset.yld + asset.div * 0.79685
+                yield_year = asset.yld + asset.div * Constants.JP_DIVIDEND_TAX_TAXABLE
             # USStock
             else:
-                yield_year = asset.yld + asset.div * 0.71787
+                yield_year = asset.yld + asset.div * Constants.US_DIVIDEND_TAX_TAXABLE
 
         k = (yield_year * (1 + yield_year) ** duration) / (
             (1 + yield_year) ** duration - 1
@@ -306,11 +363,11 @@ def get_demolition_price(assets: list[Asset], duration: int) -> dict:
                 now_price = start_price
             else:
                 now_price = now_price * (1 + yield_year) - demolition_per_year
-            price_transition[i] += now_price // 10000
+            price_transition[i] += now_price // Constants.YEN_UNIT_DIVISOR
     price_transition[-1] = 0
 
     return {
         'duration': duration,
-        'demolitionPrice': demolition_per_year_total // 10000,
+        'demolitionPrice': demolition_per_year_total // Constants.YEN_UNIT_DIVISOR,
         'priceTransition': price_transition,
     }
